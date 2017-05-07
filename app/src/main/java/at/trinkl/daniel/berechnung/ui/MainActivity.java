24677366db8ecdf828c3a3fbd62bc04a8ba640a9
stewie15 <dtrinkl015@gmail.com>
2017-05-07 19:05:18 +0200

    er findet die resource ned mehr an. hab versucht ein DropDown mit Zahlein einzufügen und seither kommt irgendwo her der Fehler. Wobei die DropDown auch noch nicht funktioniert, weil ich noch keine Werte eingefügt hab. Weiß noch nicht ganz genau wie das funktionieren soll.....

--- app/src/main/java/at/trinkl/daniel/berechnung/ui/MainActivity.java
+++ app/src/main/java/at/trinkl/daniel/berechnung/ui/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.spinner).setOnClickListener(new);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
